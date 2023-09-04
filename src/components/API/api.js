fd8deca3f5c32f11d8ebf8208c887c4745bf1781
gasanaliguliev <aligulievgasan001@gmail.com>
2023-09-04 18:23:04 +0300

    done

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiKey = '38971521-301f5cb08025e3967497dc80d';
 
 const fetchImages = async (query, page) => {
-  await axios.get(
+  return await axios.get(
     `https://pixabay.com/api/?key=${apiKey}&q=${query}&page=${page}&per_page=12`
   );
 };
